1a15fe6b8e68101a94174c8cb111b496445c6a6c
June Song <jsong@nicevan.co.kr>
2016-10-31 11:29:25 +0900

    Add testing java source by JUnit

---
+++ src/jsong00505/study/test/leetcode/LongestPalindromeTest.java
@@ -0,0 +1,14 @@
+package jsong00505.study.test.leetcode;
+
+import jsong00505.study.leetcode.no1.LongestPalindrome;
+
+import org.junit.*;
+
+public class LongestPalindromeTest {
+	@Test
+	public void test() {
+		String str = "aabb";
+		System.out.println(LongestPalindrome.longestPalindrome(str));
+	}
+
+}
