e85c845e9ea23603d9cc04443ffcd5da3b1d80e9
jsong00505 <jsong00505@gmail.com>
2016-12-19 17:13:47 +0900

    feat: add another way to hash

--- src/jsong00505/core/test/CryptographyTest.java
+++ src/jsong00505/core/test/CryptographyTest.java
@@ -8,12 +8,18 @@ import jsong00505.core.utils.Cryptography;
 
 public class CryptographyTest {
 	@Test
-	public void sh256HashTest() {
+	public void sha256HashTest() {
 		String str = "20161219161112lottewd01m10000rW/9BpWt+0YToW9yu1UQPij/GgZWx7nIThIKXMAct1pq2WIIWqQh8x+B2ebg1vpa1e7QgxqYLQUx+YoxktPKRw==";
-		System.out.println(":::LONGEST PALINDROME TEST:::");
+		String str2 = "20161219163903lottewd01m10000rW/9BpWt+0YToW9yu1UQPij/GgZWx7nIThIKXMAct1pq2WIIWqQh8x+B2ebg1vpa1e7QgxqYLQUx+YoxktPKRw==";
+		
+		System.out.println(":::SHA-256 HASH TEST:::");
 		
 		Cryptography c = new Cryptography();
 		System.out.println("SHA-256: "+c.sha256Encrypt(str));
+		System.out.println("SHA-256 2: "+c.sha256Encrypt2(str));
+		
+		System.out.println("SHA-256: "+c.sha256Encrypt(str2));
+		System.out.println("SHA-256 2: "+c.sha256Encrypt2(str2));
 		
 	}
 }
--- src/jsong00505/core/utils/Cryptography.java
+++ src/jsong00505/core/utils/Cryptography.java
@@ -1,9 +1,12 @@
 package jsong00505.core.utils;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
 import org.apache.commons.codec.binary.Base64;
+
+import sun.misc.BASE64Encoder;
 
 public class Cryptography {
 	
@@ -39,6 +42,24 @@ public class Cryptography {
 	    }
 		
 		return passACL;	// 암호화된 데이터를 리턴...
+	}
+	
+	public String sha256Encrypt2(String strData) {
+		String hashdata = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+			md.update(strData.getBytes());
+			
+			byte[] digest = md.digest();
+			
+			BASE64Encoder encoder = new BASE64Encoder();
+
+			hashdata = encoder.encode(digest);
+
+		} catch (NoSuchAlgorithmException e) {
+				
+		}
+		return hashdata;
 	}
 
 }
