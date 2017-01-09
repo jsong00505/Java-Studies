397105e765f341e2b78a9a31b3f2b8cf82bf9d19
jsong00505 <jsong00505@gmail.com>
2017-01-09 19:25:23 +0900

    chore: constants

---
+++ src/jsong/common/utils/CommonConstants.java
@@ -0,0 +1,9 @@
+package jsong.common.utils;
+
+public class CommonConstants {
+	public final static String NICEPAY_REAL_DOMAIN_URI = "https://web.nicepay.co.kr";
+	public final static String NICEPAY_STAGING_DOMAIN_URI = "https://staging.nicepay.co.kr";
+	public final static String NICEPAY_DEV_DOMAIN_URI = "https://dev.nicepay.co.kr";
+	
+	public final static String NICEPAY_BILLING_UPLOAD_API_PATH = "/api/billingUpload.jsp";
+}
