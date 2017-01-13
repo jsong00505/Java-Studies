09f0f9e65fefc1e439f8ed26cb51f9172c20f235
User <User@User-PC>
2017-01-13 11:29:19 +0900

    chore: list printing test

---
+++ src/jsong00505/study/test/chore/ListTest.java
@@ -0,0 +1,16 @@
+package jsong00505.study.test.chore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+	public static void main(String[] args) {
+		List listString = new ArrayList<>();
+		
+		listString.add("hello");
+		listString.add("bonjur");
+		
+		System.out.println(listString.toString());
+		System.out.println(listString);
+	}
+}
