62504492567ab1d7c8180e493d21c1f7e45df31e
jsong-pc <jsong00505@gmail.com>
2017-01-18 21:05:44 +0900

    feat: holding this feat. do later

---
+++ src/jsong00505/hackerrank/ds/arrays/DiretConnections.java
@@ -0,0 +1,19 @@
+package jsong00505.hackerrank.ds.arrays;
+
+import java.util.Scanner;
+
+public class DiretConnections {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int T = sc.nextInt();
+    	
+    	for(int i = 0; i < T; i++) {
+    		int N = sc.nextInt();
+    		System.out.println(N);
+    	}
+    	
+    	System.out.println("done");
+    }
+}
