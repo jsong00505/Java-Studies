0290942b06013595f4c60f6ce7a3770a1f3574bc
jsong-pc <jsong00505@gmail.com>
2017-01-20 20:06:45 +0900

    feat: Day 0 Hello,World

--- src/jsong00505/study/hack30days/Day2Operators.java
+++ src/jsong00505/study/hack30days/Day2Operators.java
@@ -1,5 +1,21 @@
 package jsong00505.study.hack30days;
 
-public class Day2Operators {
+import java.util.Scanner;
 
+public class Day2Operators {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double calCost = mealCost + mealCost * tipPercent * 0.01 + mealCost * taxPercent * 0.01;
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(calCost);
+      
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
 }
