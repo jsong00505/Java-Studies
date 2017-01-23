647f2719426ae7e3ea63426e9811298ee190bda8
jsong-pc <jsong00505@gmail.com>
2017-01-23 21:09:22 +0900

    Day5: Loops

---
+++ src/jsong00505/study/hack30days/Day5Loops.java
@@ -0,0 +1,14 @@
+package jsong00505.study.hack30days;
+
+import java.util.Scanner;
+
+public class Day5Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        for(int i = 1; i <= 9; i++) {
+        	System.out.println(String.format("%d x %d = %d", n, i, n*i));
+        }
+    }
+}
