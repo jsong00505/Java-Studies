b647646347cbc0cd53751385449ef442f4ea1489
jsong-pc <jsong00505@gmail.com>
2017-02-10 07:00:18 +0900

    feat: algorithm - binary search tree

---
+++ src/jsong00505study/hack30days/bst/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package jsong00505study.hack30days.bst;
+
+public class BinarySearchTree {
+	public static void main(String[] args) {
+		Tree<Integer> bst = new EmptyBST();
+		bst = bst.add(4);
+		bst = bst.add(3);
+		bst = bst.add(2);
+		
+		System.out.println("3 is in bst >> " + bst.member(3));
+		System.out.println("5 is in bst >> " + bst.member(5));
+		System.out.println("cardinality >> " + bst.cardinality());
+		
+	}
+}
---
+++ src/jsong00505study/hack30days/bst/NonEmptyBST.java
@@ -0,0 +1,53 @@
+package jsong00505study.hack30days.bst;
+
+public class NonEmptyBST<D extends Comparable> implements Tree<D> {
+	// we don't know whether left, right are EmptyBST or NonEmptyBST 
+	Tree<D> left;
+	Tree<D> right;
+	D element;
+	
+	public NonEmptyBST(D elt) {
+		left = new EmptyBST<D>();
+		right = new EmptyBST<D>();
+		element = elt;
+	}
+	
+	public NonEmptyBST(D elt, Tree<D> leftTree, Tree<D> rightTree) {
+		element = elt;
+		left = leftTree;
+		right = rightTree;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public int cardinality() {
+		return 1 + left.cardinality() + right.cardinality();
+	}
+	
+	public boolean member(D elt) {
+		if(element == elt) {
+			return true;
+		} else {
+			if(elt.compareTo(element) < 0) {
+				return left.member(elt);
+			} else {
+				return right.member(elt);
+			}
+		}
+	}
+	
+	public NonEmptyBST<D> add(D elt) {
+		if(element == elt) {
+			return this;
+		} else {
+			if(elt.compareTo(element) < 0) {
+				return new NonEmptyBST(element, left.add(elt), right);
+			} else {
+				return new NonEmptyBST(element, left, right.add(elt));
+			}
+		}
+		
+	}
+}
