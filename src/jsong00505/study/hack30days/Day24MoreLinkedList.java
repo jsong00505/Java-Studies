436f9ede26f92c1f24d81cdc338cc6b16ea8b7bb
jsong-pc <jsong00505@gmail.com>
2017-02-11 11:15:05 +0900

    feat: Day24 More Linked List

---
+++ src/jsong00505/study/hack30days/Day24MoreLinkedList.java
@@ -0,0 +1,69 @@
+package jsong00505.study.hack30days;
+
+import java.util.Scanner;
+
+public class Day24MoreLinkedList {
+    public static Node removeDuplicates(Node head) {
+        //Write your code here
+    	
+    	// set up data for removing duplicates
+    	Node rootNode = head;
+    	Node startNode = head;
+    	int removalData = head.data;
+
+    	
+    	// loop till head's next doen't existed
+    	while(head.next != null) {
+    		head = head.next;
+    		
+    		// check duplicates
+    		if(head.data == removalData) {
+    			startNode.next = head.next;
+    		} else {
+    			removalData = head.data;
+    			startNode = head;
+    		}
+    		
+    	}
+    	return rootNode;
+      }
+    public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+    public static void display(Node head)
+    {
+          Node start=head;
+          while(start!=null)
+          {
+              System.out.print(start.data+" ");
+              start=start.next;
+          }
+    }
+    public static void main(String args[])
+    {
+          Scanner sc=new Scanner(System.in);
+          Node head=null;
+          int T=sc.nextInt();
+          while(T-->0){
+              int ele=sc.nextInt();
+              head=insert(head,ele);
+          }
+          head=removeDuplicates(head);
+          display(head);
+
+   }
+}
