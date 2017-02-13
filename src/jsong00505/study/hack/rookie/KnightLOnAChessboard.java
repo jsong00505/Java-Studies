2c7fa06f03ccbf1d65053a13dc4d61a4f4ed59cd
jsong00505 <jsong00505@gmail.com>
2017-02-13 09:20:56 +0900

    refactor: change the way to solve ( HashMap -> int 2-D array )

--- src/jsong00505/study/hack/rookie/KnightLOnAChessboard.java
+++ src/jsong00505/study/hack/rookie/KnightLOnAChessboard.java
@@ -11,37 +11,43 @@ public class KnightLOnAChessboard {
 		
 		// count (1,1) ~ (n-1, n-1), so that would be from 2 to (n - 1) *2
 		int goal = (n - 1 ) * 2;
-		HashMap<Point, Integer> result = new HashMap<>();
+
+		int board[][] = new int[n - 1][n - 1];
 		
 		for(int i = 1; i < n; i++) {
 			for(int j = 1; j < n; j++) {
-				if(goal % (i+j) == 0) {
-					result.put(new Point(i, j), goal / (i+j));
+				if(goal % (i + j) == 0) {
+					// If goal is divisible by a+b, the quotient is minimum number of the path
+					board[i - 1][j - 1] = goal / (i + j);
 				} else {
-					result.put(new Point(i, j), -1);
+					/*
+					 * case 1. 2a
+					 * case 2. 2b
+					 * one of both would be the minimum. if not divisible by 2a or 2b, there is no minimum number
+					 * 
+					 */
+					int min = -1;
+					
+					if(goal % (2 * i) == 0) {
+						min = goal / (2 * i);
+					}
+					
+					if((goal % (2 * j) == 0) && min > (goal / (2 * j))) {
+						min = goal / (2 * j);
+					}
+					
+					board[i - 1][j - 1] = (min != -1) ? min * 2 : -1;
 				}
 			}
 			
 		}
 		System.out.println("goal : " + goal);
-		for(Point p: result.keySet()) {
-			System.out.println("("+ p.x + ","+ p.y +")" + result.get(p));
-		}
-		/*for(int i = 1; i < n; i++) {
-			for(int j = 1; j < n; j++) {
-				System.out.print(result.get(Point(i, j)) + " ");
-				
+
+		for(int i = 0; i < n - 1; i++) {
+			for(int j = 0; j < n - 1; j++) {
+				System.out.print(board[i][j] + " ");
 			}
 			System.out.print("\n");
-		}*/
-	}
-	
-	static class Point {
-		int x;
-		int y;
-		Point(int x, int y) {
-			this.x = x;
-			this.y = y;
 		}
 	}
 }
