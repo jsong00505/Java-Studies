d82e20bd1b8bbe419e82af9d563a5141ad4ca91a
jsong-pc <jsong00505@gmail.com>
2017-02-14 23:11:28 +0900

    feat: Day27 - finally understood what output should be.
    The description was bad to me...

--- src/jsong00505/study/hack30days/Day27Testing123.java
+++ src/jsong00505/study/hack30days/Day27Testing123.java
@@ -13,23 +13,45 @@ public class Day27Testing123 {
  * 5 2
  * 0 -1 2 1 4
  */
+	Day27Testing123(int n, int k, int[] a){
+        System.out.println(n + " " + k);
+        for(int i = 0; i < n; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
+        // number of test cases - each test case is two lines
+        int testCases = 5; 
+        
+        System.out.println(testCases);
+        new Day27Testing123(4, 3, new int[] {-1, 0, 4, 2});
+        new Day27Testing123(5, 2, new int[] {0, -1, 2, 1, 4});
+        new Day27Testing123(7, 6, new int[] {2, 0, -1, 1 , 1 , 1, 1});
+        new Day27Testing123(3, 1, new int[] {-1, 0, 4});
+        new Day27Testing123(6, 4, new int[] {0, -1, 1, 4, 5, 6});
+    }
+/*	public static int rndInt(int min, int max) {
+		Random rand = new Random();
+		return rand.nextInt((max-min) + 1) + min;
+	}
+    public static void main(String[] args) {
+         Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. 
 		
         Random rand = new Random();
         ArrayList<String> result = new ArrayList<>();
         
-		int numberOfTestCases = rand.nextInt(5);
+		int numberOfTestCases = rndInt(0, 5);
 		System.out.println(numberOfTestCases);
 		
 		for(int i = 0; i < numberOfTestCases; i++) {
-			int totalStudent = rand.nextInt(200);
-			int expectedOnTimeStudent = rand.nextInt(totalStudent);
+			int totalStudent = rndInt(1, 200);
+			int expectedOnTimeStudent = rndInt(1, totalStudent);
 			int countOnTimeStudent = 0;
 			System.out.println(totalStudent + " " + expectedOnTimeStudent);
 			for(int j = 0; j < totalStudent; j++) {
-				int arrivedTime = rand.nextInt(2000) - 1000;
+				int arrivedTime = rndInt(-10, 10);
 				System.out.print(arrivedTime + " ");
 				if(arrivedTime > 0) {
 					countOnTimeStudent++;
@@ -43,8 +65,8 @@ public class Day27Testing123 {
 			}
 		}
 		
-/*		for(int i = 0; i < numberOfTestCases; i++) {
+		for(int i = 0; i < numberOfTestCases; i++) {
 			System.out.println(result.get(i));
-		}*/
-    }
+		}
+    }*/
 }
