f275ee3b01dda71e7c301db379b33d5e44cb506b
jsong00505 <jsong00505@gmail.com>
2017-02-21 11:20:38 +0900

    chore: make class file for "Dorsey Thief"

---
+++ src/jsong00505/study/hack/algo/dynamic/DorseyThief.java
@@ -0,0 +1,12 @@
+package jsong00505.study.hack.algo.dynamic;
+
+import java.util.Scanner;
+
+public class DorseyThief {
+	// knapsack problem
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
