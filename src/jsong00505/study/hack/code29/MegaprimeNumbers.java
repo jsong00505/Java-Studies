cd0302d1fbe356056bf8844f637c39e57fc03c51
jsong00505 <jsong00505@gmail.com>
2017-02-24 14:14:48 +0900

    refactor: tried eratos theory, but it didn't make an algorithm better.

--- src/jsong00505/study/hack/code29/MegaprimeNumbers.java
+++ src/jsong00505/study/hack/code29/MegaprimeNumbers.java
@@ -1,5 +1,6 @@
 package jsong00505.study.hack.code29;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class MegaprimeNumbers {
@@ -8,13 +9,15 @@ public class MegaprimeNumbers {
 		long first = in.nextLong();
 		long last = in.nextLong();
 		// your code goes here
+		
 		long cnt = 0;
 		for(long i = first; i <= last; i++) {
 			if(isPrimeNumber(i)) {
 				cnt++;
 			}
 		}
-		System.out.print(cnt);
+		System.out.println(cnt);
+		System.out.print(eratos(first, last));
 	}
 
 	public static boolean isPrimeNumber(long numb) {
@@ -56,11 +59,82 @@ public class MegaprimeNumbers {
 			return false;
 		}
 
-		for(int i = 3; i <= Math.sqrt(numb); i++) {
+		for(int i = 3; i <= Math.sqrt(numb); i += 2) {
 			if(numb % i == 0) {
 				return false;
 			}
 		}
 		return true;
+	}
+	
+	public static long eratos(long first, long last) {
+		
+		HashMap<Long, Boolean> primeList = new HashMap<>();
+		
+		primeList.put((long) 1, false);
+		primeList.put((long) 2, true);
+		
+		for(long i = 3; i <= last; i += 2) {
+			primeList.put(i, true);
+		}
+		
+		for(long i = 3; i <= last; i += 2) {
+			
+			if(primeList.get(i)) {
+				for(long j = i; j * i <= last; j += 2) {
+					primeList.put(j * i, false);
+				}
+				
+				String stringNumb = Long.toString(i);
+				
+				if(stringNumb.indexOf("0") > -1) {
+					primeList.put(i, false);
+				}
+	
+				if(stringNumb.indexOf("1") > -1) {
+					primeList.put(i, false);
+				}
+	
+				if(stringNumb.indexOf("4") > -1) {
+					primeList.put(i, false);
+				}
+	
+				if(stringNumb.indexOf("6") > -1) {
+					primeList.put(i, false);
+				}
+	
+				if(stringNumb.indexOf("8") > -1) {
+					primeList.put(i, false);
+				}
+	
+				if(stringNumb.indexOf("9") > -1) {
+					primeList.put(i, false);
+				}
+			}		
+			
+		}
+	
+
+		long start = (first % 2 == 0) ? first + 1 : first;
+	
+		long cnt = 0;
+		
+		if(first <= 2) {
+			cnt++;
+		}
+		for(long i = start; i <= last; i += 2) {
+			if(primeList.get(i)) {
+				cnt++;
+			}
+		}
+		
+		return cnt;
+	}
+	
+	public static boolean isBetweenNumbers(long key, long start, long last) {
+		if(key >= start && key <= last) {
+			return true;
+		}
+		return false;
 	}
 }
