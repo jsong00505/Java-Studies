be38191ef4ce8068e248ac99da2b8f504055e00e
jsong00505 <jsong00505@gmail.com>
2017-02-25 20:13:51 +0900

    feat: Minimal Distance to PI(earned 12 points)

--- src/jsong00505/study/hack/code29/MinimalDistanceToPi.java
+++ src/jsong00505/study/hack/code29/MinimalDistanceToPi.java
@@ -11,20 +11,44 @@ public class MinimalDistanceToPi {
         // your code goes here
         
         long min = 1;
-        long max = 10;
-        double minimalDistance = -1;
-        long denominator = -1;
-        long numerator = -1;
-        for(long i = min; i <= max; i++) {
-        	long nextOfNumberThree = i * 3 + 1;
-        	System.out.println(Math.abs(nextOfNumberThree/(double)i - Math.PI));
-        	if(Math.abs(nextOfNumberThree/(double)i - Math.PI) < minimalDistance || minimalDistance == -1) {
-        		minimalDistance = Math.abs(nextOfNumberThree/i - Math.PI);
-        		
-        		numerator = nextOfNumberThree;
-        		denominator = i;
-        	}
-        }
-        System.out.println(numerator+"/"+denominator);
-    }
+        long max = (long) Math.pow(10, 1);
+		double minimalDistance = -1;
+		long denominator = -1;
+		long numerator = -1;
+		long minJ = 1;
+		for(long i = min; i <= max; i++) {
+
+			long j = minJ;
+			double temp = 0;
+			while(true) {
+				long nextOfNumberThree = i * 3 + j;
+
+				if(temp == 0) {
+					temp = Math.abs(nextOfNumberThree/(double)i - Math.PI);
+				}
+
+				if(Math.abs(nextOfNumberThree/(double)i - Math.PI) < minimalDistance || minimalDistance == -1) {
+					minimalDistance = Math.abs(nextOfNumberThree/(double)i - Math.PI);
+
+					numerator = nextOfNumberThree;
+					denominator = i;
+					if(i >= 7) {
+						minJ = j;
+					}
+
+				}
+
+				if(temp >= nextOfNumberThree/(double)i - Math.PI) {
+					temp = Math.abs(nextOfNumberThree/(double)i - Math.PI);
+					j++;
+				} else {
+					break;
+				}
+
+			}
+
+
+		}
+		System.out.println(numerator+"/"+denominator);
+	}
 }
